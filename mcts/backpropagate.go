ea5e51f329467b92fb66cf00d762d4363b5a5c68
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2024-02-16 18:02:01 +0100

    feat(wip): mcts almost complete

--- mcts/backpropagate.go
+++ mcts/backpropagate.go
@@ -1,5 +1,15 @@
 package mcts
 
-func (node *MCTSNode) Backpropagate(result int) {
+func (node *MCTSNode) Backpropagate(result uint8) {
+	// Loop to update nodes up to the root
+	for n := node; n != nil; n = n.parent {
+		n.visits += 1
+		// If the result matches the playerTurn of this node, it's a win for this node
+		if n.state.PlayerTurn == result {
+			n.wins += 1
+		}
+		// For Tic-Tac-Toe, you might also need to handle draws specifically
+		// depending on how you want to treat them in your win/loss statistics
+	}
 	// Update this node and its ancestors with the simulation result
 }
