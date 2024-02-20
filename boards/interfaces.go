f7d529efcd3e31817a13a13cd66b4764d4318ead
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2024-02-20 19:48:38 +0100

    feat: interfaces

---
+++ boards/interfaces.go
@@ -0,0 +1,38 @@
+package board
+
+// Agent is a player, usually is 1 and 2
+type Agent = uint8
+
+// Move is a possible value on a board
+type Move = uint8
+
+type Result = uint8
+
+const (
+	// Player1 ...
+	Player1 Agent = 1
+	// Player2 ...
+	Player2 Agent = 2
+	// EmptyPlace is the value on a board that has not been played yet
+	EmptyPlace Move = 0
+	// GameOn when we did not reach an end state
+	GameOn Result = 0
+	// Player1Wins
+	Player1Wins Result = 1
+	// Player2Wins
+	Player2Wins Result = 2
+	// Draw
+	Draw Result = 3
+)
+
+type Arena interface {
+	GetBoard() []Move
+	GetPlayerTurn() Agent
+}
+
+type Game interface {
+	IsGameOver() bool
+	GetWinner() Agent
+	MakeMove(Arena) Arena
+	PossibleMoves() []Arena
+}
